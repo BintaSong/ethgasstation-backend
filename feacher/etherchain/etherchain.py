4b44f1fd0ca20b5c5d1e3c38af63b1178df22583
BintaSong <bintasong@gmail.com>
2018-05-13 14:44:27 +0100

    debug

--- feacher/etherchain/etherchain.py
+++ feacher/etherchain/etherchain.py
@@ -11,12 +11,9 @@ def url_read(url):
         #print data
         counter = data['recordsTotal']
     except:
-        counter = -1
-    return counter  
+        counter = "0"
+    return counter
 
-def get_number(matched):
-    global pending_tx
-    pending_tx = int(matched.group('value'))
 
 def log(fname, content):
     f = open(fname, 'a+')
@@ -26,11 +23,10 @@ def log(fname, content):
 etherchain_url = 'https://www.etherchain.org/txs/pending/data?draw=1&columns%5B0%5D%5Bdata%5D=parenthash&columns%5B0%5D%5Bname%5D=&columns%5B0%5D%5Bsearchable%5D=true&columns%5B0%5D%5Borderable%5D=false&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=time&columns%5B1%5D%5Bname%5D=&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=false&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=from&columns%5B2%5D%5Bname%5D=&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=false&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=to&columns%5B3%5D%5Bname%5D=&columns%5B3%5D%5Bsearchable%5D=true&columns%5B3%5D%5Borderable%5D=false&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B4%5D%5Bdata%5D=value&columns%5B4%5D%5Bname%5D=&columns%5B4%5D%5Bsearchable%5D=true&columns%5B4%5D%5Borderable%5D=false&columns%5B4%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B4%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B5%5D%5Bdata%5D=gas&columns%5B5%5D%5Bname%5D=&columns%5B5%5D%5Bsearchable%5D=true&columns%5B5%5D%5Borderable%5D=false&columns%5B5%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B5%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B6%5D%5Bdata%5D=gasprice&columns%5B6%5D%5Bname%5D=&columns%5B6%5D%5Bsearchable%5D=true&columns%5B6%5D%5Borderable%5D=false&columns%5B6%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B6%5D%5Bsearch%5D%5Bregex%5D=false&start=0&length=10&search%5Bvalue%5D=&search%5Bregex%5D=false&_='
 if __name__ == '__main__':
     pending_tx = 0;
-    #pattern = re.compile(r'A total of (?P<value>\d+) Pending txns found')
     while(True):
         s = url_read( etherchain_url + str(time.time()))
         f = time.strftime("%Y-%m-%d", time.localtime())
-        #print (time.time(), s)
+        print (time.time(), s)
         log(f, str(time.time()) + ' ' + s + '\n')
         pending_tx = 0
         time.sleep(60)
--- feacher/etherscan/etherscan.py
+++ feacher/etherscan/etherscan.py
@@ -1,6 +1,5 @@
 #coding:utf-8
 import requests 
-import urllib3  
 import time
 import re
 
@@ -8,11 +7,11 @@ def url_read(url):
     try:
         return requests.get(url).text
     except:
-        return -1
+        return "-1"
 
 def get_number(matched):
     global pending_tx
-    pending_tx = int(matched.group('value'))
+    pending_tx = matched.group('value')
 
 def log(fname, content):
     f = open(fname, 'a+')
@@ -26,7 +25,7 @@ if __name__ == '__main__':
         s = url_read('https://etherscan.io/txsPending')
         re.sub('A total of (?P<value>\d+) Pending txns found', get_number, s)
         f = time.strftime("%Y-%m-%d", time.localtime())
-        #print (time.time(), pending_tx)
+        print (time.time(), pending_tx)
         log(f, str(time.time()) + ' ' + str(pending_tx) + '\n')
         pending_tx = 0
         time.sleep(60)
